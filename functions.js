b3924ca4737c4bc3ef905ed1e56c6308bbb13709
Jose Manuel <coveromaki@gmail.com>
2017-12-11 21:20:17 +0000

    Revert "Añadido soporte más o menos de ratón"
    This reverts commit 3c4d8549507da62b50d2b331ac5adaffd0eac123.

--- functions.js
+++ functions.js
@@ -3,47 +3,31 @@ var x = 0 ;
 var y = -80; 
 var c;
 var ctx ;
-var updatedImg ;
-var staticIMG;
+var img ;
+var border;
 var size = 2; 
-generarAllowed = false; //variable para ver si podemos generar o no la imagen. Previene que la imagen cargue sin darle al botón
-var SaveOriginalOffsetonce = false; //variable para guardar el offset original solo una vez
-var OriginaloffsetX; //variable para el control del ratón
-var OriginaloffsetY; //variable para el control del ratón
-var swapped;
+genearAllowed = false;
+
 function generar (){
-	generarAllowed = true;
+	genearAllowed = true;
 	update();
 } 
-function swap (){
-	aux = updatedImg;
-	updatedImg = staticIMG;
-	staticIMG = aux;
-	if(!swapped){
-			document.getElementById("buttonSwap").innerHTML = "Mover imagen";
-			swapped = true;
-		}		
-	else{
-			document.getElementById("buttonSwap").innerHTML = "Mover gorro";
-			swapped = false;
-		}
-}
 function update (){
-	if(generarAllowed){
+	if(genearAllowed){
 		ctx.clearRect(0, 0, c.width, c.height);
 
-		ctx.drawImage(updatedImg, x,y, updatedImg.width*size, updatedImg.height*size);
+		ctx.drawImage(img, x,y, img.width*size, img.height*size);
 
 
-		ctx.drawImage(staticIMG,0,0);
+		ctx.drawImage(border,0,0);
 	}
 }
-function changestaticIMG(id){
-	staticIMG = document.getElementById("r" + id );
+function changeBorder(id){
+	border = document.getElementById("r" + id );
 	update();
 }
    function previewFile(){
-       var preview = document.getElementById('photo'); //selects the query named updatedImg
+       var preview = document.getElementById('photo'); //selects the query named img
        var file    = document.querySelector('input[type=file]').files[0]; //sames as here
        var reader  = new FileReader();
 
@@ -69,11 +53,10 @@ window.onload = function(){
 	sliderY = document.getElementById("rangeY");
 	sliderSize = document.getElementById("rangeSize");
 
-	updatedImg = document.getElementById("photo");
-	staticIMG = document.getElementById("r0");
+	img = document.getElementById("photo");
+	border = document.getElementById("r0");
 
 
-	c.addEventListener("mousedown",mousedown);
 
 	//controladores de los sliders
 	sliderX.oninput = function() {
@@ -91,36 +74,4 @@ window.onload = function(){
 	  update();
 
 	}
-}
-
-function mousedown(e){
-	//alert("mousedown");
-	drag = true; 
-	c.addEventListener("mousemove",mousemove); //start dragging
-    c.addEventListener("mouseup",mouseup);
-	once = false;
-
-}
-
-function mousemove(e) {
-	console.log("moving " );
-	if(!once){
-		 OriginaloffsetX = e.offsetX;
-		 OriginaloffsetY = e.offsetY; 
-		once = true; 
-	}
-	var movementX = OriginaloffsetX - e.offsetX;
-	var movementY = OriginaloffsetY - e.offsetY;
-	x = x - movementX/10;
-	y = y - movementY/10;
-    update();
-
-	//console.log ("X: " + movementX + " Y: " + movementY);
-	//console.log(e);
-}
-function mouseup(e) {
-	drag = false;
-	c.removeEventListener("mouseup",mouseup);
-	c.removeEventListener("mousemove",mousemove);
-
 }
